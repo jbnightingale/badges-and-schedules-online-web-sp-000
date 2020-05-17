667a45e1d662a0a1384fa6128851c0e1286f8fde
Brandon Nightingale <brandon@jbnight.com>
2020-05-17 20:49:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do |name|
+      badges << "Hello, my name is #{name}."
+  end
+  badges
+end
+
+def assign_rooms(room_assignments)
+  assignments = []
+  counter = 1
+  room_assignments.each do |name|
+    assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter += 1
+  end
+  assignments
+end
